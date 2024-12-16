e9b9b4f0e2b21c2017f8b4c622f13ca68d25e5b5
CosminIoanDragusin <ioan.dragusin.cosmin@gmail.com>
2024-12-16 18:58:18 +0200

    Redefinirea Operatorilor 2

---
+++ RedefinireaOperatorilor2/RedefinireaOperatorilor2.cpp
@@ -0,0 +1,13 @@
+// RedefinireaOperatorilor2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
